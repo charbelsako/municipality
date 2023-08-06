4bcdddde986d2a10dce4112922cadf91d43b65b3
Charbel Sarkis <charbelsako@gmail.com>
2023-08-06 13:40:15 +0300

    T-7 add the base models and connection

--- server.ts
+++ server.ts
@@ -1,7 +1,22 @@
-import {config} from 'dotenv-flow'
-config()
-// import mongoose from 'mongoose'
+import { config } from 'dotenv-flow';
+config();
+import mongoose from 'mongoose';
+import express from 'express';
+import authRouter from './routes/authRouter';
 
-console.log(process.env.MONGO_URI)
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use('/auth', authRouter);
+// console.log(process.env.MONGO_URI);
 
 // connect to database
+const uri: string = process.env.MONGO_URI as string;
+mongoose.connect(uri);
+
+app.get('/', (req, res) => res.send('working'));
+
+app.listen(3000, () => console.log('Listening on Port 3000'));
