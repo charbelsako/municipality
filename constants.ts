59deb47f4802ff13d602731a116b4dce8ab8dd97
Charbel Sarkis <charbelsako@gmail.com>
2023-08-07 18:56:36 +0300

    Adds some constants to reduce repetition

---
+++ constants.ts
@@ -0,0 +1,15 @@
+import { CookieOptions } from 'express';
+
+export const statusCodes = {
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+  BAD_REQUEST: 400,
+  NO_CONTENT: 204,
+  OK: 200,
+};
+
+export const cookieOptions: CookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'none',
+};
