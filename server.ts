d351541ea68a1ad3719c75d5c53d791940d4f2d8
Charbel Sarkis <charbelsako@gmail.com>
2023-08-07 16:49:06 +0300

    Adds start:prod script and a way to serve react

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ config();
 import mongoose from 'mongoose';
 import express from 'express';
 import authRouter from './routes/authRouter';
+import path from 'path';
 
 const app = express();
 
@@ -13,10 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/auth', authRouter);
 
 // connect to database
-const uri: string = process.env.MONGO_URI as string;
+const uri = process.env.MONGO_URI as string;
 mongoose.connect(uri);
 console.log('Connect to the database');
 
-app.get('/', (req, res) => res.send('working'));
+// For React.js build
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  console.log('production mode');
+  app.use(express.static('frontend/.next/'));
+  // console.log(path.resolve(__dirname, "client/", "build/", "index.html"))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => console.log('Listening on Port 3000'));
