a860765dc2c3c3cd061b526af4672900cc230dba
Charbel Sarkis <charbelsako@gmail.com>
2023-08-12 00:24:01 +0300

    T-00 remove .js files

--- build/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_flow_1 = require("dotenv-flow");
-(0, dotenv_flow_1.config)();
-// import mongoose from 'mongoose'
-// console.log(process.env.MONGO_URI)
-// connect to database
--- build/src/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_flow_1 = require("dotenv-flow");
-(0, dotenv_flow_1.config)();
-console.log(process.env.MONGO_URI);
-// connect to database
